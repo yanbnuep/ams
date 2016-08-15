ae3ffc64229b2605039962520d8cce26567630bc
yanbnuep <yanbnuep@gmail.com>
2016-08-15 18:03:18 +0800

    point

---
+++ js/textfiled.js
@@ -0,0 +1,12 @@
+/**
+ * Created by itdwyy on 2016/8/15.
+ */
+(function(){
+    'use strict';
+
+    var AmsTextfield = function AmsTextfield(element) {
+        this.element = element;
+        this.init();
+    };
+
+})();
